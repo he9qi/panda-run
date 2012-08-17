6609fb7618b8c5b629dd43f3000a38b4158aa316
Qi He <qihe229@gmail.com>
2012-08-17 13:03:05 +0800

    over view

---
+++ panda-run/Game/Objects/Waves.h
@@ -0,0 +1,13 @@
+//
+//  Waves.h
+//  panda-run
+//
+//  Created by Qi He on 12-8-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Waves1DNode.h"
+
+@interface Waves : Waves1DNode
+
+@end
---
+++ panda-run/Game/Objects/Waves.m
@@ -0,0 +1,13 @@
+//
+//  Waves.m
+//  panda-run
+//
+//  Created by Qi He on 12-8-16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "Waves.h"
+
+@implementation Waves
+
+@end
